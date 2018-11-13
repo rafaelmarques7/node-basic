25f43c62f8b83190c0d40518797fd2b0ce80576a
rafaelmarques7 <rafaelmarques76076@gmail.com>
2018-11-13 13:01:09 +0000

    add PM2 ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       production: {
         user: 'ubuntu',
         host: 'ec2-54-229-107-58.eu-west-1.compute.amazonaws.com',
-        key: '~/.ssh/id_rsa',
+        key: '~/.ssh/engagement-host-ec2.pem',
         ref: 'origin/master',
         repo: 'git@github.com:afaelmarques7/node-basic.git',
         path: '/home/ubuntu/node-basic',
